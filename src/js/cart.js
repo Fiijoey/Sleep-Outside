56f6d80fd437426bd1d05b805d8f9d4231513fe2
LokiObahoshe <sondereggerbecca@gmail.com>
2025-03-15 15:12:04 -0500

    Added total price in cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -2,10 +2,10 @@ import { getLocalStorage } from "./utils.mjs";
 
 import { setLocalStorage } from "./utils.mjs";
 
-function removeItem(itemId){
+function removeItem(itemId) {
   let storage = getLocalStorage("so-cart");
   storage.forEach(item => {
-    if(item.Id === itemId){
+    if (item.Id === itemId) {
       let index = storage.indexOf(item);
       storage.splice(index, 1);
       setLocalStorage(storage, "so-cart");
@@ -13,24 +13,38 @@ function removeItem(itemId){
     }
   });
 }
-  
 
-function addRemoveListener(){
+
+function addRemoveListener() {
   let removeButtons = document.querySelectorAll(".remove-item");
 
-for (let index = 0; index < removeButtons.length; index++) {
-  let element = removeButtons[index];
-  element.addEventListener("click",() =>{ removeItem(element.getAttribute("data-id"))});
-  
-}
+  for (let index = 0; index < removeButtons.length; index++) {
+    let element = removeButtons[index];
+    element.addEventListener("click", () => { removeItem(element.getAttribute("data-id")) });
+
+  }
 }
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
+  const updatedCartItems = Array.isArray(cartItems) ? cartItems : [cartItems];
+
+  //const totalPrice = updatedCartItems.reduce((acc, item) => acc + (item.FinalPrice * item.quantity), 0);
+  const totalPrice = updatedCartItems.reduce((acc, item) => acc + parseFloat(item.FinalPrice || 0), 0);
+
+  const cartTotalElement = document.querySelector(".cart-total");
+
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
   addRemoveListener();
-  
+
+  if (updatedCartItems.length === 0) {
+    cartTotalElement.classList.add("hidden");
+  } else {
+    cartTotalElement.classList.remove("hidden");
+    cartTotalElement.innerHTML = `Total: $${totalPrice.toFixed(2)}`;
+  }
+
 }
 
 function cartItemTemplate(item) {
