5fdf74394f09d9b951c530d690a37d0a1525c06a
LokiObahoshe <sondereggerbecca@gmail.com>
2025-03-22 20:59:48 -0500

    fixed image issues

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -21,7 +21,7 @@ export default class ProductDetails {
 
         <img
           class="divider"
-          src="${product.Image}"
+          src="${product.Images.PrimaryLarge}"
           alt="${product.NameWithoutBrand}"
         />
 
